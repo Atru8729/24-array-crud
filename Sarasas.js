d42881c14e4f3293d0de35f83be1da4b379db9cc
Atru8729 <a.truskauskas1@gmail.com>
2021-07-09 20:13:55 +0300

    CRUD done

--- Sarasas.js
+++ Sarasas.js
@@ -12,12 +12,16 @@ class Sarasas {
     }
 
     prideti(item) {
+        if (!this.isValidItem(item)) {
+            return false;
+        }
         this.list.push(item);
     }
 
     spausdinti() {
         console.log('Sarasas:');
         console.log('===============');
+
         for (let i = 0; i < this.list.length; i++) {
             console.log((i + 1) + '. ' + this.list[i]);
 
@@ -27,12 +31,48 @@ class Sarasas {
     }
 
     redaguoti(index, newItem) {
+        if (!this.isValidIndex(index) ||
+            !this.isValidItem(newItem)) {
+            return false;
+        }
+
+        this.list[index] = newItem;
+    }
+
+    pasalinti(index) {
+        if (!this.isValidIndex(index)) {
+            return false;
+        }
+
+        const updatedList = [];
+
+        for (let i = 0; i < this.list.length; i++) {
+            if (i !== index) {
+                updatedList.push(this.list[i]);
+            }
+
+        }
+
+        this.list = updatedList;
+    }
+
+    isValidIndex(index) {
         if (this.list.length <= index ||
             index < 0 ||
             index % 1 !== 0) {
             return false;
         }
-        this.list[index] = newItem;
+
+        return true;
+    }
+
+    isValidItem(item) {
+        if (typeof item !== 'string' ||
+            item === '') {
+            return false;
+        }
+
+        return true;
     }
 }
 
